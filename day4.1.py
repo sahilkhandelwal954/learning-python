b509621877dd1a119ec26b0a0830ae5cc079cffc
Sahil Khandelwal <143314496+sahilkhandelwal954@users.noreply.github.com>
2024-05-15 15:23:02 +0530

    Add files via upload

---
+++ day4.1.py
@@ -0,0 +1,99 @@
+# learning of arthematic operator
+# num1=100
+# num2=200
+# # print(num1+num2)
+# #or
+# result=num1+num2
+# print(result)
+
+
+#user input
+
+# num1=int(input("enter the value of 1 :"))
+# num2=int(input("enter the value of 2 :"))
+# print(type(num1))
+
+
+# # result=num1+num2
+# # result=num1 ** num2   #power operator
+# # result=num1 - num2
+# result=num1 //num      #(not give float value)
+# # result=num1 / num2
+
+# print(result)
+
+
+#if,else,elif
+
+
+# num1=int(input("enter the value of 1 :"))
+# num2=int(input("enter the value of 2 :"))
+
+# if num1>num1:
+#     print("num1 is greater")
+# elif num1==num2:
+#     print("both the values are equal")
+# elif num1!=num2:
+#     print("the number is not equal")
+# else:
+#     print("num2 is greater than num1")
+
+# if num1>=num2:
+#     print('num1 is equal and greater than num2')
+# else:
+#     print('not')
+
+# age=int(input("enter the age of person"))
+# # if age>24 and age<100:
+# #     print("you can vote")
+# # else:
+# #     print("you can't vote")
+# if not age>24:
+#     print("you can vote")
+# else:
+#     print("you can't vote")
+    
+# num=1000
+# # num +=1
+# num -=666666767676676776
+# print(num)
+
+
+
+# fruit=['mango','banana','apple','cherry']
+# if "mango" in fruit:
+#     print("yes there is mango")
+# else:
+#     print("nahi ha ")
+
+
+#use of dictonary
+# dt={'banana':10,'apple':100,'cherry':23}
+
+
+        
+# if "mang" in dt:
+#     print("yes there is mango")
+# else:
+#     print("nahi ha ")
+
+
+        
+    
+    
+    
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
