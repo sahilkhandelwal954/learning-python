c4e8f31e07f63850a0729427e77925ef42a429a7
Sahil Khandelwal <143314496+sahilkhandelwal954@users.noreply.github.com>
2024-05-15 14:20:43 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,37 @@
+#introduction of the dictonary keys
+# dt={'a':10,'b':20,'c':30,'d':[1,2,3,4,]}
+# # print(dt)
+# # print(dt['d'])
+# # dt['a']=20
+# # print(dt)
+# print(dt.keys())
+# print(dt.values())
+
+
+
+
+#introduction of the sets in python 
+# st={1,1,2,2,3,3,4,4,5,5,6,6,7,7}
+# print(st)
+
+
+# type casting=changing of the data type
+# ls=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,3.4]
+# set(),int(),float(),bool(),list(),tuple()
+# print(set(ls))
+
+st={1,1,2,2,3,3,4,4,5,5,6,6,7,7}
+
+# print(set[2])
+# print(st[2])
+st.add(100)
+print(st)
+# st.remove(100)
+# print(st)
+# st.remove(101)
+# print(st)
+print("after removed",st)
+st.discard(100)
+
+print("after the removal of discard",st)
+
