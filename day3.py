bf319ff0baca41d5c8310ffdf310399e3ade6936
Sahil Khandelwal <143314496+sahilkhandelwal954@users.noreply.github.com>
2024-05-15 14:03:10 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,79 @@
+# lst=[24,'hello',True,34.45]
+# # print(lst)
+# # print(lst[::-1])
+# # lst[1]=500
+# # print(len(lst))
+# print(lst)
+# lst.append(100)
+# print(lst)
+# lst.append(100)
+
+# lst.append(102)
+
+# lst.append(103)
+
+# lst.append(104)
+# print(lst)
+# lst.pop()
+# print(lst)
+# lst.insert(1,'jecrc')
+# print(lst)
+# lst=[24,'hello',True,34.45]
+# # print(lst[3])
+# print(lst[1][-3])
+# lst=[100,22,33,44,55,66,[11,21,42,43]]
+# print(lst[6][2])
+# lst[6][2]=10000
+# print(lst)
+# tpl=(10,20,30,40,39.34,'hello',True)
+# print(tpl)
+# print(type(tpl))
+# print(tpl[2])
+# tpl[2]=200
+# college_name=('jecrc',)
+# print(college_name)
+# print(type(college_name))
+# college_name="jecrc",'pooor','van'
+# print(type(college_name))
+# print(college_name)
+# collegee="jecrc"==---
+# print(collegee)
+# print(type(collegee))
+# ar=(78,)
+# print(type(ar))
+# lst=[20,43,45,43,2,1,35,567]
+# lst.remove(43)
+# print(lst)
+# name="jecrc"
+# surname="foundation"
+# roll_no=100
+# print(name + surname)
+# print(name*5)
+# print(name+roll_no)
+# # print(name*2)
+# tpl2=(10,20,30,40)
+# print(tpl2*2)
+# lst=[1,2,3,4,5,6,7]
+# print(lst*2)
+# lst1=[1,2,3,4,5]
+# lst2=[6,7,8,9,0]
+# # lst1.append(100)
+# lst1.extend(lst2)
+# print(lst1)
+# lst1.pop()
+# print(lst1)
+
+
+#key values
+# dt={"name":"rohit kumar","branch":'cse',"roll_no":1}
+# print(dt)
+# print(type(dt))
+# print(dt["name"])
+# print(dt.keys())
+# print(dt.values())
+#dt={"name":"rohit kumar","branch":'cse',"roll_no":1}
+# dt={}
+
+class_data={"name":["a","b","c"]}
+class_data["name"]
+print(class_data["name"])
