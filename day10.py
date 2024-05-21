0b4512e22205fad20ed39217d037ff5f76e02323
Sahil Khandelwal <143314496+sahilkhandelwal954@users.noreply.github.com>
2024-05-21 11:42:23 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,6 @@
+# name=input("plz write your name:")
+# print('welcom:',name)
+#exception handling
+
+age=int(input("plz write your age :"))
+print('i am',age,'year','old')
---
+++ day8.py
@@ -0,0 +1,79 @@
+#use of while loop 
+# i=0
+# while i<=10:
+#     print(i)
+#     i+=1
+
+
+# quiet=False
+# while not  quiet:
+#     user_input=input("do you want to quiet it press it press=== ")
+#     if user_input=='yes':
+#         break
+#         # quiet=True
+#     else:
+        
+#         print("hello")
+
+
+
+
+#use of function
+#there are two type of function
+#user defined
+#built in 
+
+
+# def addtwo(num1,num2):
+#     num3=10
+#     result=num1+num2=num3
+#     # print(result)
+#     return result 
+    
+    
+    
+#     #### calling of functio
+# # output=addtwo(num1=7,num2=23)
+
+# # print(output)
+# output=addtwo(10,20)
+# print(output)
+
+# #return---->used like the break keyword
+# lst=[10,3,5,7,20,31,30,40,50,60,70,80]
+
+# # def my_len(lst):
+# #     count=0
+# #     for item in lst:
+# #         count +=1
+    
+        
+    
+# #     return count
+# # output=my_len(lst)
+# # print(output)
+
+# def even(lst):
+    
+#     count=0
+#     for item in lst:
+#         if item%2==0:
+#             count +=1
+            
+#     return count
+# output=even(lst)
+# print(output)
+
+
+
+
+# lst=[10,50,63,4,78,9,6,82,63,1,5,54,63,74,14,96,25]
+# def greater(lst):
+#     count=0
+#     for item in lst:
+#         if item>50:
+#             count +=1
+#     return count
+# output=greater(lst)
+# print(output)
+            
